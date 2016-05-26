8b43746abd1c2dca643281ee36ed4d2c35c42659
Hana Lee <i@leehana.co.kr>
2016-05-26 15:09:13 +0900

    ionic-pigdata-13 데이터 상세보기에서는 그래프로 데이터를 보여줘야 한다. * 차트 샘플 코드 추가

--- www/app/components/details/details.js
+++ www/app/components/details/details.js
@@ -3,13 +3,16 @@
  * @since 2016-05-16 17:00
  */
 
+import 'd3';
+import 'nvd3/build/nv.d3.min.js';
+import nvd3 from 'angular-nvd3';
 import detailsComponent from './details.component';
 import detailsFactory from './details.factory';
 import detailsRouter from './details.router';
 
-let homeModule = angular.module('PIGDATA.details', [])
+let homeModule = angular.module('PIGDATA.details', [nvd3])
   .config(detailsRouter)
-  .directive('details', detailsComponent)
+  .directive('itemDetails', detailsComponent)
   .factory('details.factory', detailsFactory);
 
 export default homeModule;
